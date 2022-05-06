7e326c38b899f53367ddd59845b96e537dfc62a4
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-06 12:51:39 -0300

    Hice un par de carpetas de estilos y cree la carpeta api para conectar el backend

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from "./stylesPosts";
+
+const Posts = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <h1>Posts</h1>
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
