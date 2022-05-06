569d793c2a9498a5aeb0349915b00e01fbc4a2f6
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-05-06 12:05:23 -0300

    Cree el modelo Post, su controlador y sus rutas de obtener y crear post

--- server/index.js
+++ server/index.js
@@ -3,10 +3,29 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/postsRoutes.js";
+
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/posts", postRoutes);
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://memoriesproject:memoriesproject123@cluster.aclfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
 // https://www.mongodb.com/clud/atlas
